e2b7c1a19c887a8e19f5608ae626468cee2d89a7
bgm <qkrrudals2403@naver.com>
2023-02-02 17:58:39 +0900

    예제 프로젝트 만들기 v3

--- src/main/java/hello/proxy/app/v3/OrderControllerV3.java
+++ src/main/java/hello/proxy/app/v3/OrderControllerV3.java
@@ -3,9 +3,12 @@ package hello.proxy.app.v3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
+@ResponseBody
 public class OrderControllerV3 {
 
     private final OrderServiceV3 orderService;
